f670358639288328f8aa75df1489c028b3b02489
ImXiaoYe <2423336405@qq.com>
2024-12-29 11:23:45 +0800

    1

--- run.bat
+++ run.bat
@@ -1,4 +1,3 @@
-@echo off
 .\CloudflareST.exe -n 500 -cfcolo NRT -o JP.csv -sl 10 -dn 5
 .\CloudflareST.exe -n 500 -cfcolo HKG -o HK.csv -sl 10 -dn 5
 .\CloudflareST.exe -n 500 -cfcolo KHH -o TW.csv -sl 10 -dn 5
