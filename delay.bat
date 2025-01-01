9610188bbc3222dab8d926716b459663886ff931
ImXiaoYe <2423336405@qq.com>
2025-01-01 15:33:49 +0800

    1

---
+++ delay.bat
@@ -0,0 +1,4 @@
+.\CloudflareST.exe -n 800 -cfcolo HKG -o HK.csv -dd
+.\CloudflareST.exe -n 800 -cfcolo KHH -o TW.csv -dd
+.\CloudflareST.exe -n 800 -cfcolo LAX -o US.csv -dd
+.\CloudflareST.exe -n 800 -cfcolo NRT -o JP.csv -dd
