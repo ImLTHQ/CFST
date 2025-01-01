df3a945b01bc3a92730aa1c808e1d6c8ace7d8eb
ImXiaoYe <2423336405@qq.com>
2025-01-01 15:06:33 +0800

    111

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
-.\CloudflareST.exe -n 500 -cfcolo HKG -o HK.csv -sl 10 -dn 5
-.\CloudflareST.exe -n 500 -cfcolo KHH -o TW.csv -sl 10 -dn 5
-.\CloudflareST.exe -n 500 -cfcolo LAX -o US.csv -sl 10 -dn 5
-.\CloudflareST.exe -n 500 -cfcolo NRT -o JP.csv -sl 10 -dn 5
-pause
+.\CloudflareST.exe -n 500 -cfcolo HKG -o HK.csv -sl 10 -dn 3
+.\CloudflareST.exe -n 500 -cfcolo KHH -o TW.csv -sl 10 -dn 3
+.\CloudflareST.exe -n 500 -cfcolo LAX -o US.csv -sl 10 -dn 3
+.\CloudflareST.exe -n 500 -cfcolo NRT -o JP.csv -sl 10 -dn 3
