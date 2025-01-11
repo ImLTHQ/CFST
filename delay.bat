9ed060b69f077bc113e5a4b61b32260b5d07f32f
ImXiaoYe <2423336405@qq.com>
2025-01-11 18:58:36 +0800

    1

--- delay.bat
+++ delay.bat
@@ -1,4 +1,1 @@
-.\CloudflareST.exe -n 800 -cfcolo HKG -o HK.csv -dd -tlr 0
-.\CloudflareST.exe -n 800 -cfcolo LAX -o LAX.csv -dd -tlr 0
-.\CloudflareST.exe -n 800 -cfcolo SJC -o SJC.csv -dd -tlr 0
-.\CloudflareST.exe -n 800 -cfcolo NRT -o JP.csv -dd -tlr 0
+.\CloudflareST.exe -n 800 -o CF.csv -dd -tlr 0
--- speed.bat
+++ speed.bat
@@ -1,4 +1,1 @@
-.\CloudflareST.exe -n 800 -cfcolo HKG -o HK.csv -sl 5
-.\CloudflareST.exe -n 800 -cfcolo SJC -o SJC.csv -sl 5
-.\CloudflareST.exe -n 800 -cfcolo LAX -o LAX.csv -sl 5
-.\CloudflareST.exe -n 800 -cfcolo NRT -o JP.csv -sl 5
+.\CloudflareST.exe -n 800 -o CF.csv -sl 5
